d5e58f40f87bb3c05dc32d97829e1a708e940877
gatapon <git@gatapon.net>
2018-11-27 15:41:18 +0900

    -foo controller

---
+++ app/Http/Controllers/FooController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CRUDTEST\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class FooController extends Controller
+{
+    public function foo1()
+    {
+        return 'Foo1!!';
+    }
+}
