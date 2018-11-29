4e76bb7cf834764688c1f0f40b9657a0917f6cdb
gatapon <git@gatapon.net>
2018-11-29 18:09:27 +0900

    users.indexブレード追加, index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -15,7 +15,12 @@ class UserController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::all();
+
+        return view('users.index', [
+            'users' => $users,
+        ]);
+        
     }
 
     /**
