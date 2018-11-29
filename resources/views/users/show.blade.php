6ef0ec700ae90e22c764724d244c4ea805219a4e
gatapon <git@gatapon.net>
2018-11-29 18:35:34 +0900

    users.show 作成

---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,31 @@
+@php
+    $title = __('User') . ':' . $user->name;
+@endphp
+@extends('layouts.my')
+@section('content')
+<div class="container">
+    <h1>{{ $title }}</h1>
+
+    {{-- 編集・削除ボタン --}}
+    <div>
+        <a href="{{ url('users/'.$user->id.'/edit') }}" class="btn btn-primary">
+            {{ __('Edit') }}
+        </a>
+        {{-- 削除ボタン --}}
+        <a href="#" class="btn btn-danger">
+            {{ __('Delete') }}
+        </a>
+    </div>
+
+    {{-- ユーザー1件の情報 --}}
+    <dl class="row">
+        <dt class="col-md-2">{{ __('ID') }}</dt>
+        <dt class="col-md-10">{{ $user->id }}</dt>
+        <dt class="col-md-2">{{ __('Name') }}</dt>
+        <dt class="col-md-10">{{ $user->name }}</dt>
+        <dt class="col-md-2">{{ __('E-Mail Address') }}</dt>
+        <dt class="col-md-10">{{ $user->email }}</dt>
+    </dl>
+</div>
+
+@endsection
