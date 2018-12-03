d81652fa80538ae659cd73b87c371c4c143e4db6
gatapon <git@gatapon.net>
2018-12-03 16:06:54 +0900

    post.showブレード追加

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -47,9 +47,11 @@ class PostController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show($id)
+    public function show(Post $post)
     {
-        //
+        return view('posts.show', [
+            'post' => $post
+        ]);
     }
 
     /**
