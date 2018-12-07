c9b87b5c7b650424e6221fdfd728164f935f2f0b
gatapon <git@gatapon.net>
2018-12-07 23:26:47 +0900

    migration renew

--- database/migrations/2018_11_27_171612_create_posts_table.php
+++ database/migrations/2018_11_27_171612_create_posts_table.php
@@ -15,7 +15,7 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
-            $table->integer('user_id')->unsigned()->default(1);
+            $table->integer('user_id')->unsigned()->index();
             $table->string('title');
             $table->text('body');
             $table->timestamps();
