34580a4c336588db84ad6122d838ccb9a25ce730
gatapon <git@gatapon.net>
2018-12-07 16:27:08 +0900

    管理者かの確認とユーザー自身の投稿かの確認

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -10,6 +10,18 @@ class UserPolicy
     use HandlesAuthorization;
 
     /**
+     * 編集と削除の認可を判断する。
+     * 
+     * @param  \CRUDTEST\User $user 現在ログインしているユーザー
+     * @param \CRUDTEST\User $model 現在表示しているプロフィールページのユーザー
+     * @return mixed
+     */
+    public function edit(User $userm, User $model)
+    {
+        return $user->id == $model->id;
+    }
+
+    /**
      * Create a new policy instance.
      *
      * @return void
