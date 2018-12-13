0268ab3c5af9589198a076ef971c7d675ef4dc6f
gatapon <git@gatapon.net>
2018-12-13 20:50:01 +0900

    Update create.blade.php

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -10,7 +10,7 @@
         @method('POST')
         <div class="form-group">
             <label for="title">{{ __('Title') }}</label>
-            <input id="title" type="text" class="form-control @if ($errors->has('title')) is-invalid @endif" name="title" value="{{ old('title')+ }}" required autofocus>
+            <input id="title" type="text" class="form-control @if ($errors->has('title')) is-invalid @endif" name="title" value="{{ old('title') }}" required autofocus>
             @if ($errors->has('title'))
                 <span class="invalid-feedback" role="alert">
                     {{ $errors->first('title') }}
@@ -19,7 +19,7 @@
         </div>
         <div class="form-group">
             <label for="body">{{ __('Body') }}</label>
-            <textarea id="body" class="form-control @if ($errors->has('body')) is-invalid @endif" name="body" rows="8" required>{{ old('body')+ }}</textarea>
+            <textarea id="body" class="form-control @if ($errors->has('body')) is-invalid @endif" name="body" rows="8" required>{{ old('body') }}</textarea>
             @if ($errors->has('body'))
                 <span class="invalid-feedback" role="alert">
                     {{ $errors->first('body') }}
