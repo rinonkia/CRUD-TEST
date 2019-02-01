b6ea536b877ad19bb248dfa6e87b943ce04e70ba
gatapon <git@gatapon.net>
2019-02-01 15:07:02 +0900

    import VerifyEmail

--- app/Notifications/CustomVerifyEmail.php
+++ app/Notifications/CustomVerifyEmail.php
@@ -6,6 +6,7 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Notifications\Notification;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class CustomVerifyEmail extends Notification
 {
